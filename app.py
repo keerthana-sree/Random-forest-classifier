5048656dc93db0a81c5242f4f4938a6697c57f51
keerthana-sree <sreekee72@gmail.com>
2026-01-02 14:55:00 +0530

    file added

--- app.py
+++ app.py
@@ -1,35 +1,25 @@
-import streamlit as st
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 import pickle
-import numpy as np
 
-st.set_page_config(page_title="ML Prediction App", page_icon="🚢")
+# Load data
+df = pd.read_csv("titanic.csv")
 
-st.title("🚢 Titanic Survival Prediction")
-st.write("Enter passenger details to predict survival")
+# Select EXACT 6 features
+X = df[['Pclass', 'Sex', 'Age', 'Fare', 'SibSp', 'Parch']]
 
+# Encode sex
+X['Sex'] = X['Sex'].map({'male': 1, 'female': 0})
 
-@st.cache_resource
-def load_model():
-    with open("titanic_model.pkl", "rb") as file:
-        model = pickle.load(file)
-    return model
+# Fill missing values
+X.fillna(X.mean(), inplace=True)
 
-model = load_model()
+y = df['Survived']
 
-pclass = st.selectbox("Passenger Class", [1, 2, 3])
-sex = st.selectbox("Sex", ["Male", "Female"])
-age = st.slider("Age", 1, 80, 25)
-fare = st.slider("Fare", 0.0, 500.0, 50.0)
+# Train model
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X, y)
 
-
-sex = 1 if sex == "Male" else 0
-
-
-if st.button("Predict"):
-    input_data = np.array([[pclass, sex, age, fare]])
-    prediction = model.predict(input_data)
-
-    if prediction[0] == 1:
-        st.success("✅ Passenger is likely to SURVIVE")
-    else:
-        st.error("❌ Passenger is NOT likely to survive")
+# Save model
+with open("titanic_model.pkl", "wb") as f:
+    pickle.dump(model, f)
